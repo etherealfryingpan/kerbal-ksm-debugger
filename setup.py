655b2300ce9a5f3e56df4f682930ceeba5f3f864
newcomb-luke <newcomb.luke@protonmail.com>
2020-04-05 19:31:43 -0400

    Updated setup.py for cx_Freeze

--- setup.py
+++ setup.py
@@ -1,12 +1,7 @@
-from distutils.core import setup
-import py2exe
+from cx_Freeze import setup, Executable
 
 setup(name='ksm_debugger',
       version='0.1.0',
-      packages=['my_project'],
-      entry_points={
-          'console_scripts': [
-              'my_project = my_project.__main__.main'
-          ]
-      },
+      packages=['ksm_debugger'],
+      executables = [Executable('ksm_debugger\\__main__.py', targetName='ksm_debugger.exe')]
       )
